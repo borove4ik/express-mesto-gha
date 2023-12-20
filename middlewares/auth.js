c3b35ef3a5c1fc07dce691c9def644546df2456e
Denis <123562174+borove4ik@users.noreply.github.com>
2023-12-20 21:37:11 +0300

    перенос авторизации на cookies

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,14 +4,10 @@ const UnauthorizedError = require('../errors/unauthorized');
 module.exports = (req, res, next) => {
   let payload;
   try {
-    const { authorization } = req.headers;
-    if ((authorization && authorization.startsWith('Bearer '))) {
-      const token = authorization.replace('Bearer ', '');
-      payload = jwt.verify(token, 'secret-key');
-      req.user = payload;
-      return next();
-    }
-    return next(new UnauthorizedError('Неверные авторизационные данные 14'));
+    const token = req.cookies._id;
+    payload = jwt.verify(token, 'secret-key');
+    req.user = payload;
+    return next();
   } catch (error) {
     return next(new UnauthorizedError('Неверные авторизационные данные'));
   }
